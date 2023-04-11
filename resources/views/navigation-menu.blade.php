fb0dad6b5bad8bc8ef4df9af1510e42dbd04b96e
oladejiafo <69392408+oladejiafo@users.noreply.github.com>
2023-04-11 18:51:11 +0400

    login

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,14 +6,15 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                       <img src="{{ asset('images/logo-plain.png') }}" alt="">
+                        <!-- <x-jet-application-mark class="block h-9 w-auto" /> -->
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
                     </x-jet-nav-link>
                 </div>
             </div>
