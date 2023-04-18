596e0e3eff043fcde3324400739ab807392f5aaf
oladejiafo <dejigegs@gmail.com>
2023-04-18 16:51:19 +0400

    tariff

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Permission\Models\Role;
+// use Spatie\Permission\Models\Role;
 
 class Role extends Model
 {
